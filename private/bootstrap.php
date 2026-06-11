24e277f32261c1bf1439d8b5481a139c31620446
dealexanders <144049618+dealexanders@users.noreply.github.com>
2026-06-11 14:35:09 +0200

    Update bootstrap.php

--- private/bootstrap.php
+++ private/bootstrap.php
@@ -6,6 +6,7 @@ require_once __DIR__ . '/database.php';
 require_once __DIR__ . '/responses.php';
 require_once __DIR__ . '/forecasts.php';
 require_once __DIR__ . '/telegram.php';
+require_once __DIR__ . '/scoring.php';
 
 $config = require __DIR__ . '/config.php';
 
